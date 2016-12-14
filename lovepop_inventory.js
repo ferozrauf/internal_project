30331837544377205ac293d2678173536a4ed934
ferozrauf <frauf24@hotmail.com>
2016-12-14 03:06:43 -0500

    writing to file and fixed callback

--- lovepop_inventory.js
+++ lovepop_inventory.js
@@ -10,63 +10,55 @@ var app     = express();
 
 var url = 'https://www.lovepopcards.com/products';
 var cart_url = 'https://www.lovepopcards.com/cart/add.js';
+var stream = fs.createWriteStream('data.csv');
 
 var products = new Array();
 
 var i = 0;
+var size = 0;
 
 
-function popUpCardsPostRequest(error,response,body)
+function popUpCardsPostRequest(product)
 {
-	try {
-		var overfill_response = JSON.parse(body);
-		var num_items = overfill_response.description.indexOf('sold out') !== -1 ? 0 : parseInt(overfill_response.description.match(/\d+/)[0]);
-		var item_name = overfill_response.description.indexOf('sold out') !== -1 ? overfill_response.description.replace('The product \'','').replace('\' is already sold out.','') :  overfill_response.description.replace('You can only add ' + num_items + ' ','').replace(' to the cart.','');
-		products[i].num_items = num_items;
-		products[i].item_name = item_name;
-		//console.log(products[i]);
+	return function(error,response,body) {
 		//console.log(body);
-		//console.log(i);
-		i++;
-	} catch(e) {
-		setTimeout(function() {
-			request(
-			{
-				url:cart_url,
-				method:'POST',
-				async:false,
-				headers: {
-					'Accept-Encoding' : 'gzip, deflate, br',
-					'Accept-Language' : 'en-US,en;q=0.8',
-					'User-Agent' : 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/54.0.2840.99 Safari/537.36', 
-					'Content-Type' : 'application/x-www-form-urlencoded',
-					'Accept' : 'application/json, text/javascript, */*; q=0.01',
-					'Referer' : products[i].url.toString(),
-					'X-Requested-With' : 'XMLHttpRequest',
-					'Connection' : 'keep-alive',
-					'Origin' : 'https://www.lovepopcards.com'
-				},
-				body: 'id=' + products[i].id + '&quantity=9999999999' 
-			},popUpCardsPostRequest);
-		},1000);
-		
-	}
-		
-		//console.log(overfill_response);
-		//console.log(num_items);
-		//console.log(item_name);
-		//console.log(i);
-		
-		//console.log(products[i]);
-		//console.log(body);
-		//console.log(i);
-		//i++;
-		/*setTimeout(function() {
+		//console.log(product);
+		try {
+			var overfill_response = JSON.parse(body);
+			var num_items = overfill_response.description.indexOf('sold out') !== -1 ? 0 : parseInt(overfill_response.description.match(/\d+/)[0]);
+			var item_name = overfill_response.description.indexOf('sold out') !== -1 ? overfill_response.description.replace('The product \'','').replace('\' is already sold out.','') :  overfill_response.description.replace('You can only add ' + num_items + ' ','').replace(' to the cart.','');
+			product.num_items = num_items;
+			product.item_name = item_name;
+			stream.write(product.url.replace('https://www.lovepopcards.com/products/','') + ',' + product.id + ',' + product.num_items + '\n'); 
 			i++;
-		},100);*/
-		//console.log(response);
+			if(i===size-1)
+			{
+				stream.close();
+				process.exit();
+			}
+		} catch(e) {
+			setTimeout(function() {
+				request(
+				{
+					url:cart_url,
+					method:'POST',
+					headers: {
+						'Accept-Encoding' : 'gzip, deflate, br',
+						'Accept-Language' : 'en-US,en;q=0.8',
+						'User-Agent' : 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/54.0.2840.99 Safari/537.36', 
+						'Content-Type' : 'application/x-www-form-urlencoded',
+						'Accept' : 'application/json, text/javascript, */*; q=0.01',
+						'Referer' : product.url.toString(),
+						'X-Requested-With' : 'XMLHttpRequest',
+						'Connection' : 'keep-alive',
+						'Origin' : 'https://www.lovepopcards.com'
+					},
+					body: 'id=' + product.id + '&quantity=9999999999' 
+				},popUpCardsPostRequest(product));
+			},1000);
+		}
+	}
 	
-	//var item_name = overfill_response.description 
 }
     // The structure of our request call
     // The first parameter is our URL
@@ -80,6 +72,7 @@ function loadDataFromLovePop(error,reponse,html)
        	url = url.replace('/products','');
        	var	urls = $('a.grid__image');
        	var ids = $('select');
+       	size = urls.length;
 		for(var j=0; j<urls.length;j++)
 		{
 			//console.log(x[i].attribs.href);
@@ -89,13 +82,14 @@ function loadDataFromLovePop(error,reponse,html)
 			products.push(un_prod);
 		} 
    	}
+
    	for(j=0; j < products.length;j++)
    	{
+   		//console.log(products[j]);
    		request(
 		{
 			url:cart_url,
 			method:'POST',
-			async:false,
 			headers: {
 				'Accept-Encoding' : 'gzip, deflate, br',
 				'Accept-Language' : 'en-US,en;q=0.8',
@@ -108,12 +102,18 @@ function loadDataFromLovePop(error,reponse,html)
 				'Origin' : 'https://www.lovepopcards.com'
 			},
 			body: 'id=' + products[j].id + '&quantity=9999999999' 
-		},popUpCardsPostRequest);
+		},popUpCardsPostRequest(products[j]));
    	}
 	
 }
 
-request(url, loadDataFromLovePop);
+
+stream.once('open', function(fd) {
+	stream.write('Product Name, ID, Quantity\n');
+	request(url, loadDataFromLovePop);
+});
+
+
 
 
 
